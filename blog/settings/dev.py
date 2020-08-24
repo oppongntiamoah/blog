a8cc9ea4d7ba5ea83d124e06243cfbde97ce852f
Emmanuel Ntiamoah <ntiamoahone123@gmail.com>
2020-08-24 10:39:13 +0000

    deploy sett

--- blog/settings/dev.py
+++ blog/settings/dev.py
@@ -1,10 +1,11 @@
 from .base import *
+import django_heroku
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'u8od8lr1b18-zmz^gpv*7hg+xt5fgyr$cr8r#nr43a2jajd*12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -18,3 +19,14 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
